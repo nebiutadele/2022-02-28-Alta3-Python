d374d043eb14bbc05da264df23bce1dc03f7c5df
Nebiu Tadele <nebiutadele@gmail.com>
2022-03-03 17:11:40 +0000

    join and split

---
+++ python_challenge/python-challenge01.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+#Defining challenge function
+def challenge():
+
+    #user provides input
+    user_input = input("How are you feeling today?\n")
+    
+    # display the input back to the user.
+    print("You are  " + user_input )
+    
+# this calls the newly created function
+challenge()
