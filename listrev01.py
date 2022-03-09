fa3ff3adb1503335f7dd9be6902710bd0b90075c
Nebiu Tadele <nebiutadele@gmail.com>
2022-03-09 19:45:29 +0000

    learning about lists

---
+++ listrev01.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Learning or Reviewing about Lists | by Alta3 Researcg"""
+
+def main():
+    # create an empty list
+    myemptylist = []
+
+    # add to our list with a list method extend()
+    myemptylist.extend('192.168.102.55')
+
+    # display the list
+    print(myemptylist)
+
+if __name__ == "__main__":
+    main()
---
+++ listrev02.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Learning or Reviewing about Lists | by Alta3 Research"""
+
+def main():
+    anotheremptylist = []
+
+    # This will throw an ERROR, extend() expects one argument
+    anotheremptylist.extend('10.0.0.1', 'reto_gamer_server')
+    print(anotheremptylist)
+
+if __name__ == "__main__":
+    main()
